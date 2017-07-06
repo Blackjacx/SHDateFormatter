4dce963e4e31a805bd839fbfb2f607b41ad487e8
Stefan Herold <stefan.herold@gmail.com>
2017-07-06 13:50:19 +0200

    refactored tests to better see what fails

--- Tests/DateFormatterSpec.swift
+++ Tests/DateFormatterSpec.swift
@@ -55,95 +55,7 @@ class DateFormatterSpec: QuickSpec {
         return date
     }
 
-    lazy var testDates: [Date] = {
-        return [
-            DateFormatterSpec.dateFrom(year: 2000, month: 1, day: 1, hour: 0, min: 0, sec: 0), // 12 am a Saturday
-            DateFormatterSpec.dateFrom(year: 2000, month: 1, day: 2, hour: 12, min: 0, sec: 0), // 12 pm a Sunday
-            DateFormatterSpec.yesterdayNoon(),
-            DateFormatterSpec.todayNoon(),
-            DateFormatterSpec.tomorrowNoon(),
-        ]
-    }()
-
     override func spec() {
-
-        let expectationsForDate: [Date : [String : [SHDateFormat : String]]] = [
-            /*
-             * Testing relative date midnight
-             */
-            testDates[0] : [
-                "de_DE" : [
-                    .shortWeekdayName:          "Sa",
-                    .longWeekdayName:           "Samstag",
-                    .shortTimeNoDate:           "00:00",
-                    .noTimeShortDateNoYear :    "1.1.",
-                    .noTimeShortDate:           "1.1.00",
-                    .noTimeLongDate:            "1. Januar 2000",
-                    .noTimeRelativeDate:        "1. Januar 2000",
-                    .ISO8601:                   "1999-12-31T23:00:00Z",
-                ],
-
-                "en_EN" : [
-                    .shortWeekdayName:          "Sat",
-                    .longWeekdayName:           "Saturday",
-                    .shortTimeNoDate:           "12:00 AM",
-                    .noTimeShortDateNoYear :    "1/1",
-                    .noTimeShortDate:           "1/1/00",
-                    .noTimeLongDate:            "January 1, 2000",
-                    .noTimeRelativeDate:        "January 1, 2000",
-                    .ISO8601:                   "1999-12-31T23:00:00Z",
-                ],
-
-                "fr_FR" : [
-                    .shortWeekdayName:          "sam.",
-                    .longWeekdayName:           "samedi",
-                    .shortTimeNoDate:           "00:00",
-                    .noTimeShortDateNoYear :    "01/01",
-                    .noTimeShortDate:           "01/01/00",
-                    .noTimeLongDate:            "1 janvier 2000",
-                    .noTimeRelativeDate:        "1 janvier 2000",
-                    .ISO8601:                   "1999-12-31T23:00:00Z",
-                ],
-            ],
-
-            /*
-             * Testing noon
-             */
-            testDates[1] : [
-                "de_DE" : [
-                    .shortWeekdayName:          "So",
-                    .longWeekdayName:           "Sonntag",
-                    .shortTimeNoDate:           "12:00",
-                    .noTimeShortDateNoYear :    "2.1.",
-                    .noTimeShortDate:           "2.1.00",
-                    .noTimeLongDate:            "2. Januar 2000",
-                    .noTimeRelativeDate:        "2. Januar 2000",
-                    .ISO8601:                   "2000-01-02T11:00:00Z",
-                ],
-
-                "en_EN" : [
-                    .shortWeekdayName:          "Sun",
-                    .longWeekdayName:           "Sunday",
-                    .shortTimeNoDate:           "12:00 PM",
-                    .noTimeShortDateNoYear :    "1/2",
-                    .noTimeShortDate:           "1/2/00",
-                    .noTimeLongDate:            "January 2, 2000",
-                    .noTimeRelativeDate:        "January 2, 2000",
-                    .ISO8601:                   "2000-01-02T11:00:00Z",
-                ],
-
-                "fr_FR" : [
-                    .shortWeekdayName:          "dim.",
-                    .longWeekdayName:           "dimanche",
-                    .shortTimeNoDate:           "12:00",
-                    .noTimeShortDateNoYear :    "02/01",
-                    .noTimeShortDate:           "02/01/00",
-                    .noTimeLongDate:            "2 janvier 2000",
-                    .noTimeRelativeDate:        "2 janvier 2000",
-                    .ISO8601:                   "2000-01-02T11:00:00Z",
-                ],
-            ],
-            ]
 
         describe("DateFormatter") {
 
@@ -162,7 +74,275 @@ class DateFormatterSpec: QuickSpec {
             it("Verifies that daylight saving time is on") {
                 expect(type(of: self).timeZone.isDaylightSavingTime()) == true
             }
-            
+
+            context("Test 2000/01/01/12:00am") {
+
+                let testDate = type(of: self).dateFrom(year: 2000, month: 1, day: 1, hour: 0, min: 0, sec: 0) // 12 am a Saturday
+
+                context("\(deDE_Locale.identifier)") {
+
+                    it("ShortWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortWeekdayName, locale: deDE_Locale)
+                        expect(result) == "Sa"
+                    }
+
+                    it("LongWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .longWeekdayName, locale: deDE_Locale)
+                        expect(result) == "Samstag"
+                    }
+
+                    it("ShortTimeNoDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortTimeNoDate, locale: deDE_Locale)
+                        expect(result) == "00:00"
+                    }
+
+                    it("NoTimeShortDateNoYear") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDateNoYear, locale: deDE_Locale)
+                        expect(result) == "1.1."
+                    }
+
+                    it("NoTimeShortDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDate, locale: deDE_Locale)
+                        expect(result) == "1.1.00"
+                    }
+
+                    it("NoTimeLongDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeLongDate, locale: deDE_Locale)
+                        expect(result) == "1. Januar 2000"
+                    }
+
+                    it("NoTimeRelativeDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: deDE_Locale)
+                        expect(result) == "1. Januar 2000"
+                    }
+
+                    it("ISO8601") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .ISO8601, locale: deDE_Locale)
+                        expect(result) == "1999-12-31T23:00:00Z"
+                    }
+                }
+
+                context("\(enEN_Locale.identifier)") {
+
+                    it("ShortWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortWeekdayName, locale: enEN_Locale)
+                        expect(result) == "Sat"
+                    }
+
+                    it("LongWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .longWeekdayName, locale: enEN_Locale)
+                        expect(result) == "Saturday"
+                    }
+
+                    it("ShortTimeNoDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortTimeNoDate, locale: enEN_Locale)
+                        expect(result) == "12:00 AM"
+                    }
+
+                    it("NoTimeShortDateNoYear") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDateNoYear, locale: enEN_Locale)
+                        expect(result) == "1/1"
+                    }
+
+                    it("NoTimeShortDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDate, locale: enEN_Locale)
+                        expect(result) == "1/1/00"
+                    }
+
+                    it("NoTimeLongDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeLongDate, locale: enEN_Locale)
+                        expect(result) == "January 1, 2000"
+                    }
+
+                    it("NoTimeRelativeDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: enEN_Locale)
+                        expect(result) == "January 1, 2000"
+                    }
+
+                    it("ISO8601") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .ISO8601, locale: enEN_Locale)
+                        expect(result) == "1999-12-31T23:00:00Z"
+                    }
+                }
+
+                context("\(frFR_Locale.identifier)") {
+
+                    it("ShortWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortWeekdayName, locale: frFR_Locale)
+                        expect(result) == "sam."
+                    }
+
+                    it("LongWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .longWeekdayName, locale: frFR_Locale)
+                        expect(result) == "samedi"
+                    }
+
+                    it("ShortTimeNoDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortTimeNoDate, locale: frFR_Locale)
+                        expect(result) == "00:00"
+                    }
+
+                    it("NoTimeShortDateNoYear") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDateNoYear, locale: frFR_Locale)
+                        expect(result) == "01/01"
+                    }
+
+                    it("NoTimeShortDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDate, locale: frFR_Locale)
+                        expect(result) == "01/01/00"
+                    }
+
+                    it("NoTimeLongDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeLongDate, locale: frFR_Locale)
+                        expect(result) == "1 janvier 2000"
+                    }
+
+                    it("NoTimeRelativeDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: frFR_Locale)
+                        expect(result) == "1 janvier 2000"
+                    }
+
+                    it("ISO8601") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .ISO8601, locale: frFR_Locale)
+                        expect(result) == "1999-12-31T23:00:00Z"
+                    }
+                }
+            }
+
+            context("Test 2000/01/02/12:00pm") {
+
+                let testDate = type(of: self).dateFrom(year: 2000, month: 1, day: 2, hour: 12, min: 0, sec: 0) // 12 pm a Sunday
+
+                context("\(deDE_Locale.identifier)") {
+
+                    it("ShortWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortWeekdayName, locale: deDE_Locale)
+                        expect(result) == "So"
+                    }
+
+                    it("LongWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .longWeekdayName, locale: deDE_Locale)
+                        expect(result) == "Sonntag"
+                    }
+
+                    it("ShortTimeNoDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortTimeNoDate, locale: deDE_Locale)
+                        expect(result) == "12:00"
+                    }
+
+                    it("NoTimeShortDateNoYear") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDateNoYear, locale: deDE_Locale)
+                        expect(result) == "2.1."
+                    }
+
+                    it("NoTimeShortDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDate, locale: deDE_Locale)
+                        expect(result) == "2.1.00"
+                    }
+
+                    it("NoTimeLongDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeLongDate, locale: deDE_Locale)
+                        expect(result) == "2. Januar 2000"
+                    }
+
+                    it("NoTimeRelativeDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: deDE_Locale)
+                        expect(result) == "2. Januar 2000"
+                    }
+
+                    it("ISO8601") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .ISO8601, locale: deDE_Locale)
+                        expect(result) == "2000-01-02T11:00:00Z"
+                    }
+                }
+
+                context("\(enEN_Locale.identifier)") {
+
+                    it("ShortWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortWeekdayName, locale: enEN_Locale)
+                        expect(result) == "Sun"
+                    }
+
+                    it("LongWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .longWeekdayName, locale: enEN_Locale)
+                        expect(result) == "Sunday"
+                    }
+
+                    it("ShortTimeNoDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortTimeNoDate, locale: enEN_Locale)
+                        expect(result) == "12:00 PM"
+                    }
+
+                    it("NoTimeShortDateNoYear") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDateNoYear, locale: enEN_Locale)
+                        expect(result) == "1/2"
+                    }
+
+                    it("NoTimeShortDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDate, locale: enEN_Locale)
+                        expect(result) == "1/2/00"
+                    }
+
+                    it("NoTimeLongDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeLongDate, locale: enEN_Locale)
+                        expect(result) == "January 2, 2000"
+                    }
+
+                    it("NoTimeRelativeDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: enEN_Locale)
+                        expect(result) == "January 2, 2000"
+                    }
+
+                    it("ISO8601") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .ISO8601, locale: enEN_Locale)
+                        expect(result) == "2000-01-02T11:00:00Z"
+                    }
+                }
+
+                context("\(frFR_Locale.identifier)") {
+
+                    it("ShortWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortWeekdayName, locale: frFR_Locale)
+                        expect(result) == "dim."
+                    }
+
+                    it("LongWeekdayName") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .longWeekdayName, locale: frFR_Locale)
+                        expect(result) == "dimanche"
+                    }
+
+                    it("ShortTimeNoDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .shortTimeNoDate, locale: frFR_Locale)
+                        expect(result) == "12:00"
+                    }
+
+                    it("NoTimeShortDateNoYear") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDateNoYear, locale: frFR_Locale)
+                        expect(result) == "02/01"
+                    }
+
+                    it("NoTimeShortDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeShortDate, locale: frFR_Locale)
+                        expect(result) == "02/01/00"
+                    }
+
+                    it("NoTimeLongDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeLongDate, locale: frFR_Locale)
+                        expect(result) == "2 janvier 2000"
+                    }
+
+                    it("NoTimeRelativeDate") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: frFR_Locale)
+                        expect(result) == "2 janvier 2000"
+                    }
+
+                    it("ISO8601") {
+                        let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .ISO8601, locale: frFR_Locale)
+                        expect(result) == "2000-01-02T11:00:00Z"
+                    }
+                }
+            }
+
             context("Test Yesterday Noon") {
 
                 let testDate = type(of: self).yesterdayNoon()
@@ -291,20 +471,6 @@ class DateFormatterSpec: QuickSpec {
                     it("NoTimeRelativeDate") {
                         let result = SHDateFormatter.shared.stringFromDate(date: testDate, format: .noTimeRelativeDate, locale: frFR_Locale)
                         expect(result) == "demain"
-                    }
-                }
-            }
-
-            context("for all locales") {
-                it("formats dates correctly") {
-                    for (testDate, expectationsForLocaleID) in expectationsForDate {
-                        for (localeID, expectationsForFormat) in expectationsForLocaleID {
-                            for (format, expectation) in expectationsForFormat {
-                                let locale = Locale(identifier: localeID)
-                                let actualResult = SHDateFormatter.shared.stringFromDate(date: testDate, format: format, locale: locale)
-                                XCTAssertEqual(actualResult, expectation, "Failed for locale '\(localeID)' format '\(format)' and testDate '\(testDate)'")
-                            }
-                        }
                     }
                 }
             }
