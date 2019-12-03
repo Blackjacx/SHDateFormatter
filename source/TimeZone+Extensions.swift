d8740bc35593c321bc402a0b14776d464512c258
Stefan Herold <stefan.herold@gmail.com>
2019-12-03 13:20:02 +0100

    Publish TimeZone Extension

--- source/TimeZone+Extensions.swift
+++ source/TimeZone+Extensions.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 // swiftlint:disable force_unwrapping
-extension TimeZone {
+public extension TimeZone {
 
     static let GMT = TimeZone(identifier: "GMT")!
     
     enum Europe {
-        static let Berlin = TimeZone(identifier: "Europe/Berlin")!
-        static let London = TimeZone(identifier: "Europe/London")!
+        public static let Berlin = TimeZone(identifier: "Europe/Berlin")!
+        public static let London = TimeZone(identifier: "Europe/London")!
     }
 }
